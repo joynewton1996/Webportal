2250fd5841f833487bda126355952a9ec904ea3c
roy-einstein <32195027+roy-einstein@users.noreply.github.com>
2021-05-13 10:45:39 +0530

    initial commit

---
+++ manager.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Create an application instance."""
+from app import create_app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run()
---
+++ run.sh
@@ -0,0 +1,1 @@
+gunicorn manager:app -b 0.0.0.0:5000 -w 4
